e12b5cc8fe3fc3635c76abd2cf33484086e47aeb
Extra-codes <byaxyoxojayev@gmail.com>
2023-06-05 07:21:46 +0500

    Cool project

--- watchlistapp/api/urls.py
+++ watchlistapp/api/urls.py
@@ -1,14 +1,30 @@
 # from watchlistapp.api.views import movie_details, movie_list
 # from watchlistapp.api.views import MovieDetailAV, MovieListAV
-from watchlistapp.api.views import StreamPlatformAV, WatchListAV, WatchDetailAV, StreamPlatformDetailAV
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from watchlistapp.api.views import (ReviewCreate, ReviewDetail,ReviewList,
+                                    StreamPlatformAV, StreamPlatformVS,WatchListAV, 
+                                    WatchDetailAV,StreamPlatformDetailAV)
+from django.urls import path, include
+
+
+router = DefaultRouter()
+router.register('stream', StreamPlatformVS, basename='streamplatform')
+
+
 
 urlpatterns = [
     # path('list/', MovieListAV.as_view(), name = 'movie_list'),
     # path('list/<int:pk>', MovieDetailAV.as_view(), name = 'single-movie'),
     path('list/', WatchListAV.as_view(), name = 'movie_list'),
     path('list/<int:pk>', WatchDetailAV.as_view(), name = 'single-movie'),
-    path('stream/', StreamPlatformAV.as_view(), name = 'stream'),
-    path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name = 'streamplatform-detail'),
+    
+    path('', include(router.urls)),
+    # path('stream/', StreamPlatformAV.as_view(), name = 'stream'),
+    
+    # path('review/', ReviewList.as_view(), name = 'review-list'),
+    # path('review/<int:pk>', ReviewDetail.as_view(), name = 'review-detail'),
+    path('stream/<int:pk>/review-create', ReviewCreate.as_view(), name = 'review-create'),
+    path('stream/<int:pk>/review', ReviewList.as_view(), name = 'review-list'),
+    path('stream/review/<int:pk>', ReviewDetail.as_view(), name = 'review-detail'),
     
 ]
--- watchlistapp/api/views.py
+++ watchlistapp/api/views.py
@@ -1,19 +1,101 @@
 # from watchlistapp.api.serializers import MovieSerializer
-from watchlistapp.api.serializers import WatchlistSerializer, StreamPlatformSerializer
-from watchlistapp.models import Watchlist, StreamPlatform
+from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import ValidationError
+from watchlistapp.api.serializers import (ReviewSerializer,
+WatchlistSerializer, StreamPlatformSerializer)
+from watchlistapp.models import Review, Watchlist, StreamPlatform
 # from watchlistapp.models import Movie
 # from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import generics
+# from rest_framework import mixins
 from rest_framework.views import APIView
+from rest_framework import viewsets
 
 
+class ReviewCreate(generics.CreateAPIView):
+    serializer_class = ReviewSerializer
+    
+    def get_queryset(self):
+        return Review.objects.all()
+    
+    def perform_create(self, serializer):
+        pk = self.kwargs.get('pk')
+        watchlist = Watchlist.objects.get(pk=pk)
+        
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=watchlist, review_user=review_user)
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed")
+        serializer.save(watchlist=watchlist, review_user=review_user)
+
+class ReviewList(generics.ListAPIView):
+    # queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    
+    
+    def get_queryset(self):
+        pk = self.kwargs['pk']
+        return Review.objects.filter(watchlist=pk)
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer    
+
+# class ReviewDetail(mixins.RetrieveModelMixin,
+#                    generics.GenericAPIView):
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+    
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+# class ReviewList(mixins.ListModelMixin,
+#                   mixins.CreateModelMixin,
+#                   generics.GenericAPIView):
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+    
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+
+class StreamPlatformVS(viewsets.ModelViewSet):
+    queryset = StreamPlatform.objects.all()
+    serializer_class = StreamPlatformSerializer
+
+
+# class StreamPlatformVS(viewsets.ViewSet):
+    
+#     def list(self, request):
+#         queryset = StreamPlatform.objects.all()
+#         serializer = StreamPlatformSerializer(queryset, many=True)
+#         return Response(serializer.data)
+    
+#     def retrieve(self, request, pk=None):
+#         queryset = StreamPlatform.objects.all()
+#         watchlist = get_object_or_404(queryset, pk=pk)
+#         serializer = StreamPlatformSerializer(watchlist)
+#         return Response(serializer.data)
+
+#     def create(self, request):
+#         serializer = StreamPlatformSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 class StreamPlatformAV(APIView):
 
     def get(self, request):
         platform = StreamPlatform.objects.all()
         """This request was be when HyperLinked was created"""
-        serializer = StreamPlatformSerializer(platform, many=True, context={'request': request})
+        serializer = StreamPlatformSerializer(platform, many=True)
         # serializer = StreamPlatformSerializer(platform, many=True)
         return Response(serializer.data)
 
